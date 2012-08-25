55c30ed410273372771c33d358341341555938e7
Jerome Eteve <jerome@eteve.net>
2012-08-25 19:45:31 +0100

    Added an introduction

--- lib/Context.pm
+++ lib/Context.pm
@@ -8,6 +8,52 @@ use Moose;
 
 Context - A config key holder.
 
+=head1 INTRODUCTION
+
+Context is a framework that aims at solving the problem of storing configuration properties accross
+an ever growing collection of contexts that often characterises enterprise systems.
+
+For instance, you might want to have a 'page colour' setting that is global to your system,
+but allow users to choose their own if they want.
+
+Additionally, you might want to allow your users to specifically define a page color when
+they view a specific 'list of stuff' in your system. Or allow the system to specify a page color
+for all lists, or a specific one to certain lists, but still allowing users to override that.
+
+Multiplication of preferences and management of their priorities can cause a lot of confusion
+and headaches. This module is an attempt to help you to keep those things tidy and in control.
+
+=head1 SYNOPSIS
+
+To use Context, the best way is probably to use a Context::Manager that will
+keep your contexts tidy for you.
+
+  my $cm = Context::Manager->new();
+  $cm->universe()->set_property('page.colour' , 'blue');
+
+  my $users = $cm->restrict('users');
+  $users->set_property('page.colour', 'green');
+
+  my $user1 = $cm->restrict('users' , 1);
+  $user1->set_property('page.colour' , 'red');
+
+
+  $user1->get_property('page.colour'); # red
+
+  my $user2 = $cm->restrict('users' , 2);
+  $user2->get_property('page.colour') ; # green
+
+  my $lists = $cm->restrict('lists');
+  my $list1 = $cm->restrict->($lists, 1);
+
+  my $u1l1 = $cm->unite($user1, list1);
+  $u1l1->set_property('page.colour', 'purple');
+
+  $u1l1->get_property('page.colour'); # purple
+
+  my $u1l2 = $cm->unite($user1 , $cm->restrict('lists' , 2));
+  $u1l2->get_property('page.colour') ; # red
+
 =head1 VERSION
 
 Version 0.01
